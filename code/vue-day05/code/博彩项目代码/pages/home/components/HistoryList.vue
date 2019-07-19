1266fd501008d6c78758f778f35d7d607552ab7d
pengjim520golang <84207336@qq.com>
2019-07-19 12:27:13 +0800

    vuejs

--- code/vue-day05/code/博彩项目代码/pages/home/components/HistoryList.vue
+++ code/vue-day05/code/博彩项目代码/pages/home/components/HistoryList.vue
@@ -1,6 +1,12 @@
 <template>
     <div class="div-padding">
-        <history-item v-for="item in items" :key="item.code" :history="item"></history-item>
+       
+        <history-item  
+           v-for="item in items" 
+           :key="item.code" 
+           :history="item"
+           @click.native="gotoDetailsPage('/details?code=' + item.code)">
+        </history-item>
     </div>
 </template>
 
@@ -12,6 +18,13 @@ export default {
     props:["items"],
     components:{
         HistoryItem
+    },
+    methods:{
+        gotoDetailsPage(path){
+            //$router实际上叫全局的路由对象，来自于vue-router的模块
+            //$route表示私有组件的路由对象
+            this.$router.push(path)  
+        }
     }
 }
 </script>
